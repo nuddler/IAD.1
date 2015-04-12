845add4e7ecccd5e26b6386106347f3b9d2168d8
Przemysław Holak <180557@edu.p.lodz.pl>
2015-04-12 18:21:44 +0200

    dasdsan

--- MultiLayerPerceptronProject/src/main/java/MultiLayerPerceptronProject.java
+++ MultiLayerPerceptronProject/src/main/java/MultiLayerPerceptronProject.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author nuddler
@@ -10,9 +14,11 @@ public class MultiLayerPerceptronProject {
 	static List<Double> points = new ArrayList<Double>();
 	static MultiLayerPerceptron multiLayerPerceptron;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		List<Double> inputList = null;
+		ArrayList<ArrayList<Double>> readInputFromFile = readInputFromFile();
+		
+/*		List<Double> inputList = null;
 		
 		int hiddenLayerNeuronCount = 0;
 		int outputLayerNeuronCount = 0;
@@ -21,7 +27,26 @@ public class MultiLayerPerceptronProject {
 		
 		multiLayerPerceptron = new MultiLayerPerceptron(inputList,hiddenLayerNeuronCount,inputLayerNeuronCount,outputLayerNeuronCount,lerningFactory);
 		
-		multiLayerPerceptron.learn();
+		multiLayerPerceptron.learn();*/
 	}
-
+	
+	public static ArrayList<ArrayList<Double>> readInputFromFile() throws FileNotFoundException{
+		ArrayList<ArrayList<Double>> input = new ArrayList<ArrayList<Double>>();
+		Scanner scanner = new Scanner(new File("input"));
+		ArrayList<Double> row = new ArrayList<Double>();
+		
+		int i = 1;
+		while(scanner.hasNextDouble()){			
+			row.add(scanner.nextDouble());
+			
+			if (i%4==0){
+				ArrayList<Double> temp = new ArrayList<Double>(row);
+				input.add(temp);
+				row.clear();
+			}
+			i++;
+		}
+		return input;	
+	}
 }
+
